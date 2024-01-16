09d2ce5f092cb3b7e456eb5f4befa44261e60a0e
Thi Phan <thiphan94@gmail.com>
2024-01-16 12:51:51 +0100

    read csv

--- src/etl.py
+++ src/etl.py
@@ -1,5 +1,6 @@
 import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, StringType, LongType,FloatType, TimestampType, ShortType, DateType, BooleanType
 
 #create SparkSession
 def initialize_Spark():
@@ -11,4 +12,33 @@ def initialize_Spark():
 
     return spark
 
-#read CSV
+#Reading CSV without using Schema
+def loadDFWithoutSchema(spark):
+
+    df = spark.read.format("csv").option("header", "true").load("../data/london.csv")
+
+    return df
+
+#Reading CSV using user-defined Schema
+def loadDFWithSchema(spark):
+
+    schema = StructType([
+        StructField("timestamp", DateType(), True),
+        StructField("cnt", LongType(), True),
+        StructField("t1", FloatType(), True),
+        StructField("t2", FloatType(), True),
+        StructField("hum", FloatType(), True),
+        StructField("wind_speed", FloatType(), True),
+        StructField("weather_code", LongType(), True),
+        StructField("is_holiday", BooleanType(), True),
+        StructField("season", LongType(), True)
+    ])
+
+    df = spark \
+        .read \
+        .format("csv") \
+        .schema(schema)         \
+        .option("header", "true") \
+        .load("../data/london.csv")
+
+    return df

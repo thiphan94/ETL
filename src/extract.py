c877280bc7f8f1df2093278f68c3efd2bfbb9852
Thi Phan <thiphan94@gmail.com>
2024-01-17 07:01:18 +0100

    extract updated

--- src/extract.py
+++ src/extract.py
@@ -3,17 +3,18 @@ from constant import SCHEMA
 
 # Reading CSV without using Schema
 def loadDFWithoutSchema(spark):
-    df = spark.read.format("csv").option("header", "true").load("../data/train.csv")
+    df = spark.read.format("csv").option("header", "true").load("../ETL/data/train.csv")
 
     return df
 
 
+# Reading CSV with using Schema
 def loadDFWithSchema(spark):
     df = (
         spark.read.format("csv")
         .schema(SCHEMA)
         .option("header", "true")
-        .load("../data/train.csv")
+        .load("../ETL/data//train.csv")
     )
 
     return df
